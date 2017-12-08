65da579f195bd0638fd2fd387f1172383232ef75
Omar Sahl <omar753sahl@gmail.com>
2017-12-08 21:54:07 +0200

    first commit

---
+++ src/main/java/ui/App.java
@@ -0,0 +1,24 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("main_screen.fxml"));
+        primaryStage.setTitle("QS Models");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/ui/MainScreenController.java
@@ -0,0 +1,9 @@
+package ui;
+
+/**
+ * Created by Omar
+ * Date: 07-Dec-17.
+ */
+public class MainScreenController {
+
+}
