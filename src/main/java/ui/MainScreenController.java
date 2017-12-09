0a2082cd3bf9b53970f10abcf9312a1e0cb8413f
Omar Sahl <omar753sahl@gmail.com>
2017-12-09 22:46:52 +0200

    main screen design.

--- src/main/java/ui/MainScreenController.java
+++ src/main/java/ui/MainScreenController.java
@@ -1,9 +1,45 @@
 package ui;
 
-/**
- * Created by Omar
- * Date: 07-Dec-17.
- */
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MainScreenController {
 
+    @FXML
+    private JFXButton mm1Button;
+
+    @FXML
+    private JFXButton mmkButton;
+
+    @FXML
+    private JFXButton mmcButton;
+
+    @FXML
+    private JFXButton mmckButton;
+
+    @FXML
+    public void initialize() {
+        System.out.println("Done loading FXML components!");
+    }
+
+    @FXML
+    void onMm1Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMmcClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMmckClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMmkClicked(ActionEvent event) {
+
+    }
 }
