7136bfdc089a4db888acbab1b8201677c917d765
Omar Sahl <omar753sahl@gmail.com>
2017-12-18 17:49:46 +0200

    cleanup div branch

--- src/main/java/ui/OutputScreenController.java
+++ src/main/java/ui/OutputScreenController.java
@@ -1,7 +1,5 @@
 package ui;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
@@ -12,10 +10,26 @@ import models.QueueType;
 import java.io.IOException;
 
 public class OutputScreenController {
-
-
     private QueueType queueType;
     private QueueSystemInput inputs;
+
+    @FXML
+    private BorderPane root;
+
+    @FXML
+    private Label labelL;
+
+    @FXML
+    private Label labelLq;
+
+    @FXML
+    private Label labelW;
+
+    @FXML
+    private Label labelWq;
+
+    @FXML
+    private Label backButton;
 
     public void initView(QueueType queueType, QueueSystemInput inputs) {
         this.queueType = queueType;
@@ -24,27 +38,6 @@ public class OutputScreenController {
         System.out.println("QueueType: " + queueType.name());
         System.out.println(inputs);
     }
-
-    @FXML
-    private BorderPane root;
-
-    @FXML
-    private JFXTextField arrivalRateField;
-
-    @FXML
-    private JFXTextField serviceRateField;
-
-    @FXML
-    private JFXTextField numberOfServersField;
-
-    @FXML
-    private JFXTextField systemCapacityField;
-
-    @FXML
-    private JFXButton submitButton;
-
-    @FXML
-    private Label backButton;
 
     @FXML
     void onBackClicked(MouseEvent event) throws IOException {
