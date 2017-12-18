6479e0122c23fb35490581e35bd1197770f314f5
conancode96 <conancode96@gmail.com>
2017-12-18 18:39:21 +0200

    Added first stochastic model (M/M/1)

--- src/main/java/models/MM1QueueModel.java
+++ src/main/java/models/MM1QueueModel.java
@@ -1,5 +1,19 @@
 package models;
 
-public class MM1QueueModel {
-    
+public class MM1QueueModel extends QueueModel{
+
+    @Override
+    protected PerformanceMetrics calculatePerformanceMetrics(QueueSystemInput input) throws QueueModelException {
+
+        double lambda = input.getArrivalRate();
+        double mue = input.getServiceRate();
+
+        double L = lambda / (mue - lambda);
+        double Lq = L * lambda / mue;
+
+        double W = 1 / (mue - lambda);
+        double Wq = W * lambda / mue;
+
+        return new PerformanceMetrics(L, Lq, W, Wq);
+    }
 }
